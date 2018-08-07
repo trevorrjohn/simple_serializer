c72cc55ebfd0ce47c188c7ee3cc3706236568f40
Trevor John <trevor@john.tj>
2018-08-07 13:00:54 -0400

    Update benchmark to test serializing new instances for each object

--- benchmark.rb
+++ benchmark.rb
@@ -8,57 +8,33 @@ Author = Struct.new(:id, :name, :publisher, :books)
 Book = Struct.new(:id, :title, :date)
 
 NewInstancesSimpleSerialzier = Class.new(SimpleSerializer) do
-  define_method(:serialize_collections) do |hash|
-    self.class.collections.each do |collection_name, key, serializer, block|
-      collection = get_collection(collection_name, block)
+  define_method(:serializable_hash) do |_ = nil|
+    return nil if @object.nil?
+    return serialize_single_object_to_hash unless collection?
 
-      json_array = []
-
-      collection.each do |object|
-        serializer_instance = serializer.new(object, options)
-        json_array << serializer_instance.serialize
-      end
-
-      hash[key] = json_array
+    @object.map do |object|
+      self.class.new(object, @options).serializable_hash
     end
   end
-end
-
-class PublisherSerializer < SimpleSerializer
-  attributes :id, :name, :website_url
 end
 
 class BookSerializer < SimpleSerializer
   attributes :id, :title, :date
 end
 
-class AuthorSerializer < SimpleSerializer
-  attributes :id, :name
-
-  has_one :publisher
-  has_many :books
+class BookNewInstanceSerializer < NewInstancesSimpleSerialzier
+  attributes :id, :title, :date
 end
-
-class AuthorNewInstanceSerializer < NewInstancesSimpleSerialzier
-  attributes :id, :name
-
-  has_one :publisher
-  has_many :books
-end
-
-publisher = Publisher.new('1', 'Penguin Books', 'penguin.com')
 
 books = Array.new(10_000) do |n|
   Book.new(n.to_s, "Book #{n}", Date.new(2014, 8, 5))
 end
 
-author = Author.new('1', 'James McBride', publisher, books)
-
 Benchmark.bmbm do |x|
   x.report("existing serializer:") do
-    AuthorSerializer.new(author).serializable_hash
+    BookSerializer.new(books).serializable_hash
   end
   x.report("new instance serializer:") do
-    AuthorNewInstanceSerializer.new(author).serializable_hash
+    BookNewInstanceSerializer.new(books).serializable_hash
   end
 end
